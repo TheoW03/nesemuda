13e5b960a2cb475029a202cf26a9e59e0582937a
TheoW03 <21theowillis@gmail.com>
2025-02-22 18:46:42 -0500

    fixed a slight bug :3

--- src/instruction_disam.cpp
+++ src/instruction_disam.cpp
@@ -2,10 +2,18 @@
 #include <memory>
 #include <instruction.h>
 
+std::vector<uint8_t> diasm_addressmode(AddressMode addressMode, DisAsmState &disasm)
+{
+    std::vector<uint8_t> ret;
+
+    if (addressMode == AddressMode::IMMEDIATE)
+    {
+        ret.push_back(disasm.bus.get_instr());
+        return ret;
+    }
+}
 std::shared_ptr<instr> LDA(AddressMode addressMode, DisAsmState &disasm)
 {
-    uint8_t data = disasm.bus.get_instr();
-    std::vector<uint8_t> data_vec;
-    data_vec.push_back(data);
+    std::vector<uint8_t> data_vec = diasm_addressmode(addressMode, disasm);
     return std::make_shared<Lda>(addressMode, data_vec);
 }
