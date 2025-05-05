1dd5d32eb3122a2041491d7eb35ce59b46c2ebbb
TheoW03 <21theowillis@gmail.com>
2025-05-05 15:51:51 -0400

    added new test cases

--- src/TestRoms/TestRom1.s
+++ src/TestRoms/TestRom1.s
@@ -16,77 +16,84 @@
 
 reset:
 
-    LDA #42        ; Immediate Mode: Load the value $42 into A
-    LDA $2001       ; Absolute Mode: Load value from memory address $2000
-    LDA $20         ; Zero Page Mode: Load value from address $0020
-    LDA $20,X       ; Zero Page,X Mode: Load from address $0020 + X
-    LDA $2000, X     ; Absolute,X Mode: Load from $2000 + X
-    LDA $2000,Y     ; Absolute,Y Mode: Load from $2000 + Y
-    LDA ($20,X)     ; Indirect,X Mode: Load from address stored at ($20 + X)
-    LDA ($20),Y 
-  lda test
-  ; jmp test
-  ; jsr test
-  ldx #1
-  ldy #1
-  jsr test
-  jmp test
-      .byte $12, $32, $22, $32
+;     LDA #42        ; Immediate Mode: Load the value $42 into A
+;     LDA $2001       ; Absolute Mode: Load value from memory address $2000
+;     LDA $20         ; Zero Page Mode: Load value from address $0020
+;     LDA $20,X       ; Zero Page,X Mode: Load from address $0020 + X
+;     LDA $2000, X     ; Absolute,X Mode: Load from $2000 + X
+;     LDA $2000,Y     ; Absolute,Y Mode: Load from $2000 + Y
+;     LDA ($20,X)     ; Indirect,X Mode: Load from address stored at ($20 + X)
+;     LDA ($20),Y 
+;   lda test
+;   ; jmp test
+;   ; jsr test
+;   ldx #1
+;   ldy #1
+;   jsr test
+;   jmp test
+;       .byte $12, $32, $22, $32
 
-  ; jsr test
-  ; .byte $12, $32, $22, $32 T
+;   ; jsr test
+;   ; .byte $12, $32, $22, $32 T
 
-; reset:
+; ; reset:
 
-    ; jsr test
+;     ; jsr test
 
-;     ; LDY #1
-;     ; LDY $20
-;     ; LDY $20, X
-;     ; LDY $2000
-;     ; LDY $2000, X
+; ;     ; LDY #1
+; ;     ; LDY $20
+; ;     ; LDY $20, X
+; ;     ; LDY $2000
+; ;     ; LDY $2000, X
 
 
-;     ; LDX #1
-;     ; LDX $20
-    ; LDX $20, Y
-;     ; LDX $2000
-;     ; LDX $2000, Y
+; ;     ; LDX #1
+; ;     ; LDX $20
+;     ; LDX $20, Y
+; ;     ; LDX $2000
+; ;     ; LDX $2000, Y
 
-;     clc
-;     sec
-;     cld
-;     sed
-;     cli
-;     sei
-;     clv
+; ;     clc
+; ;     sec
+; ;     cld
+; ;     sed
+; ;     cli
+; ;     sei
+; ;     clv
 
-;     jmp reset 
-;     l3: 
-;       lda #1
-;       ; rti
-;       ; jmp l2
-;       L4:
-;       bne L4
-;       bpl nmi
-;       bcc nmi
-;       bcs nmi
-;       bmi nmi
-;       jmp l2
-;       ; lda #1
-;     ; .byte $82, $29
-;     ; .byte $1
+; ;     jmp reset 
+; ;     l3: 
+; ;       lda #1
+; ;       ; rti
+; ;       ; jmp l2
+; ;       L4:
+; ;       bne L4
+; ;       bpl nmi
+; ;       bcc nmi
+; ;       bcs nmi
+; ;       bmi nmi
+; ;       jmp l2
+; ;       ; lda #1
+; ;     ; .byte $82, $29
+; ;     ; .byte $1
+; nmi:
+; lda #1
+; lda #2
+; lda #1
+jmp test
 nmi:
-lda #1
-lda #2
+lda t2
+; jmp test2
 rti
 t2:
   lda #32
-test:
   lda #11
-  beq t2
+test:
+  lda #64
+  jmp t2
 
-
+test2:
+  jmp test
 
 ; l2:      
 ;   lda $01
